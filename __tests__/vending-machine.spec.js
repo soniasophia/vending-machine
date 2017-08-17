783caf355aff53e9005dd14cac1d078545075d66
Sonia <soniasophiaw@gmail.com>
2017-08-17 16:04:48 -0700

    Initial commit

---
+++ __tests__/vending-machine.spec.js
@@ -0,0 +1,9 @@
+const { VendingMachine } = require('../src/vending-machine');
+
+describe('Vending Machine', () => {
+  
+  describe('Printing Inventory', () => {
+    
+    it('should print the name and quantity of the products')
+  })
+})
---
+++ src/vending-machine.js
@@ -0,0 +1,87 @@
+const Treats = {
+  chips: {
+    1: {
+      id: 1,
+      name: "Chips",
+      cost: 1.00,
+      quantity: 10
+    }
+  },
+  soda: {
+    2: {
+      id: 2,
+      name: "Soda",
+      cost: 1.25,
+      quantity: 10
+    }
+  },
+  candy: {
+    3: {
+      id: 3,
+      name: "Candy",
+      cost: 1.50,
+      quantity: 10
+    }
+  },
+  nuts: {
+    4: {
+      id: 4,
+      name: "Nuts",
+      cost: 2.50,
+      quantity: 10
+    }
+  }
+}
+
+const Money = {
+  dollar: {
+    1: {
+      id: 1,
+      name: "Dollar",
+      value: 1.00
+    }
+  },
+  toonie: {
+    2: {
+      id: 2,
+      name: "Toonie",
+      value: 2.00
+    }
+  },
+  quarter: {
+    3: {
+      id: 3,
+      name: "Quarter",
+      value: 0.25
+    }
+  },
+  dime: {
+    4: {
+      id: 4,
+      name: "Dime",
+      value: 0.10
+    }
+  },
+  nickel: {
+    5: {
+      id: 5,
+      name: "Nickel",
+      value: 0.05
+    }
+  }
+}
+
+
+
+class VendingMachine {
+  constructor() {
+
+
+  }
+}
+
+
+
+module.exports = {
+  VendingMachine
+}
